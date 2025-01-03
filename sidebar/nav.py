239927023db9204537cd5cbbcd950e054e804005
Ariel Kuetche <stevekamga18@gmail.com>
2025-01-03 13:39:43 +0100

    page title

--- sidebar/nav.py
+++ sidebar/nav.py
@@ -4,7 +4,7 @@ import streamlit as st
 def Navbar():
     with st.sidebar:
             st.image("images/app_nlp.png", use_container_width=True)
-            st.page_link('home.py', label='Home', icon='🏠')
+            st.page_link('app.py', label='Home', icon='🏠')
             st.page_link('pages/generate.py', label='Générateur de texte', icon='📝')
             st.page_link('pages/sentence.py', label='Sentence Similarity', icon='📊')
             st.page_link('pages/sommarisation.py', label='Sommarisation', icon='🔍')
